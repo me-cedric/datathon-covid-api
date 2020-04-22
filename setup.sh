913b95aba79ca414114251c4a356a3a1ab81c26f
Romain Muller <com@oprax.fr>
2020-04-22 22:06:36 +0200

    add setup

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt remove docker docker-engine docker.io containerd runc
+
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    git \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker $USER
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+git submodule update --init --recursive
+
+docker-compose up -d --build
